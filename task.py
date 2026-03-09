8aebb9fd323c05e7defa7e5d5a78c2ab385f0e89
TarasMysiuraTutor <tarasmysiuratutor@gmail.com>
2026-03-09 19:17:05 +0200

    Create task.py

---
+++ task.py
@@ -0,0 +1,16 @@
+print("Сьогодні у нас заняття з Python.")
+# # name = input("Please enter your name: ")
+# # age = int(input("Please enter your age: "))
+# print(name, age)
+# print(f"Hello, {name}! You are {age} years old.")
+# # print(type(name))
+# # print(type(age))
+
+# a = 9
+# b = 3
+# c = a/b
+# print(int(c))
+print('Знаходження відстаніза вдомим часомта швидкістю руху об\'єкта')
+t = float(input('Введіть час (у годинах): '))
+v = float(input('Введіть швидкість (у км/год): '))
+print(f'Відстань, яку пройде об\'єкт за {t} годин зі швидкістю {v} км/год, дорівнює {t*v} км.')
